26426168a1fa2d10481bd628c2fcb249a1ec9a3d
Daniyal Amiri <daniyalx77@gmail.com>
2025-03-07 17:27:39 +0330

    add package laravel permition and laravel logs

---
+++ database/migrations/2025_03_07_132504_create_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedMediumInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('key');
+            $table->text('value');
+            $table->boolean('is_global')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
